08e852828affebdba5db6c9d32009f6037064055
kit0-0 <kt1767@gmail.com>
2023-05-15 00:27:11 +0530

    Update Navlist onclick

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const menu = document.getElementById('nav-elements');
 const menuOpenClose = document.getElementById('menu');
 const logo = document.getElementById('logo');
+const mobileNavList = document.querySelectorAll('.mobile-nav-list');
 
 function togglerMenu() {
   menu.classList.toggle('active');
@@ -9,15 +10,6 @@ function togglerMenu() {
   logo.classList.toggle('hide');
 }
 
-// Add click event listener to each mobile menu link
-document.querySelectorAll('.mobile-nav-list a').forEach((link) => {
-  link.addEventListener('click', (event) => {
-    event.preventDefault();
-    const targetId = link.getAttribute('href');
-    const targetSection = document.querySelector(targetId);
-    if (targetSection) {
-      targetSection.scrollIntoView({ behavior: 'smooth' });
-      togglerMenu();
-    }
-  });
+mobileNavList.forEach((element) => {
+  element.addEventListener('click', togglerMenu);
 });
